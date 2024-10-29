d1e08204ec25325327e8d68e383b025d166e5f61
nicolas-m-romero <117554210+nicolas-m-romero@users.noreply.github.com>
2024-10-28 19:12:53 -0500

    Began checkout screen, and completed navbar

--- panda_kiosk/src/app/checkout/page.js
+++ panda_kiosk/src/app/checkout/page.js
@@ -1,13 +1,8 @@
 import Image from "next/image";
+import Navbar from "../components/Navbar";
 
 export default function Checkout() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="w-full md:max-w-prose p-4">
-        <h1 className="text-9xl font-extrabold my-8">Order & Pay Here</h1>
-        <h2 className="text-5xl font-extrabold my-8">Tap Anywhere To Start!</h2>
-        <img src="Panda.jpg" alt="Panda" className="rounded-lg" />
-      </div>
-    </div>
-  );
+    return (
+        <Navbar />
+    );
 }
--- panda_kiosk/src/app/page.js
+++ panda_kiosk/src/app/page.js
@@ -1,11 +1,22 @@
-import Image from "next/image";
+"use client";
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+
+  // Function to handle click and route to Checkout page
+  const handleClick = () => {
+    router.push('/checkout');
+  };
+
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div 
+      onClick={handleClick}
+      className="flex bg-red-600 items-center justify-center min-h-screen cursor-pointer"
+    >
       <div className="w-full md:max-w-prose p-4">
-        <h1 className="text-9xl font-extrabold my-8">Order & Pay Here</h1>
-        <h2 className="text-5xl font-extrabold my-8">Tap Anywhere To Start!</h2>
+        <h1 className="text-9xl text-white font-extrabold my-8">Order & Pay Here</h1>
+        <h2 className="text-5xl text-white font-extrabold my-8">Tap Anywhere To Start!</h2>
         <img src="Panda.jpg" alt="Panda" className="rounded-lg" />
       </div>
     </div>
